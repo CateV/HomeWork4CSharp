6a5cc43c29b2b1fd7a916b596e261ac45cb31499
Kate V <katsiarynait@gmail.com>
2023-02-12 12:57:23 +0300

    готово, есть вопрос

---
+++ Task 3.MassiveOfElements/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray (int []array)
+{
+    for(int i=0;i<array.Length;i++)
+    array[i]=new Random().Next(1,10);
+}
+void PrintArray (int[] massive)
+{
+for(int i=0;i<massive.Length;i++)
+System.Console.Write($"{massive[i]}\t");
+}
+int[] arr = new int [8];
+FillArray(arr);
+PrintArray(arr);
